f638f7982d5a39d7ade3191cbe46796087345a60
Prayas <prayaspal04@gmail.com>
2025-05-02 15:49:15 +0530

    layout config

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,23 +1,26 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Providers from "./_providers/provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SealiX - Blockchain-Verified Credentials",
-  description: "Earn blockchain-verified badges and NFT diplomas for your skills and achievements.",
-}
+  title: "SealiX",
+  description: "Decentralised Skill Badges",
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
